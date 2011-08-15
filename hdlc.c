f6f471afa7630e38281d2f40b8ea1143a9777c14
Benoit Modave <vadom.benoit@gmail.com>
2011-08-16 00:13:52 +0200

    refactor hdlc.c

--- hdlc.c
+++ hdlc.c
@@ -23,8 +23,8 @@ bits control;
 
 //For int to char
 typedef union combo {
-	unsigned int Int;	   
-	unsigned char Char[2];	   
+	unsigned int Int;		
+	unsigned char Char[2];		
 } Tcombo;
 
 //Receiving charters
@@ -52,6 +52,7 @@ Tcombo crc;
 //function prototype's
 void hdlc_checkFrame(void);
 void hdlc_parseFrame(void);
+void hdlc_sendData(void);
 
 void hdlc_init() {
 	//Read address in EEPROM.
@@ -71,13 +72,13 @@ void hdlc_init() {
 	//Set up Tx & Rx pins of UART
 	TRISC|= 0xC0;
 	ANSELC&= 0x3F;
-	
 	//Set up Receive-/Transmit enable pins, as output
 	TRISC&= 0b11001111;
 	LATC&= 0xCF; //Transmit enable, receive disable
 	
 	//Set up RS485 output led
 	TRISD&= 0b11110111;
+	
 	LATD&=0xF7; //Disable led
 	
 	//Enable UART interrupts
@@ -131,74 +132,119 @@ void hdlc_transmit() {
 	}
 }
 
-//Add data to transmission buffer, and if data = DLE when it isn't a header, escape it.
-void hdlc_sendbuffer(unsigned char data, char headers) {
+//Add data to transmission buffer, and if data equals DLE when it isn't a header, escape it.
+void hdlc_sendbuffer(unsigned char data, char header) {
 	//Set data in transmission buffer
 	transmit_buffer[head]= data;
 	head++;
 	
-	if ( (data == DLE) && (headers == 0)) {  //Do not escape DLE when it is in the header!
+	if ( (data == DLE) && (header == 0)) {  //Do not escape DLE when it is in the header!
 		//Set DLE in transmission buffer
 		transmit_buffer[head]= DLE;
 		head++;
 	}
 }
 
-//Parse incomming data and act accordingly.
+//Parse incomming data and act accordingly. ( HDLC )
 void hdlc_parseFrame() {  //What do I have to do?
-	if ( (received_data[1]&0xC0) == 0xC0u) { //Unnumbered Frame
-		if ( (received_data[1]&0x37) == 0x01u) { //U-F, set normal response mode
+	if ((received_data[1]&0xC0) == 0xC0u) { //Unnumbered Frame
+		if ( (received_data[1]&0x37) == 0x01u) { //U-F: SNRM => Set Normal Response Mode
 			receive_sequence_number= send_sequence_number= 0; //init sequence numbers to zero
 			control.NRM= 1u; //Now we are in Normal Response Mode
 			if ( (received_data[1]&0x08) == 0x08u) { //Check poll flag
 				// Send Unnumbered acknowledge
-				head= tail= 0; //reset buffer
+				//Calculate CRC
+				crc.Int= 0xffff;
+				crc.Int= crc_1021(crc.Int, address);
+				crc.Int= crc_1021(crc.Int, 0xCE);
+				
+				head= tail= 0; //Reset send buffer
 				hdlc_sendbuffer(DLE, 1);
 				hdlc_sendbuffer(STX, 1);
 				hdlc_sendbuffer(address, 0);
 				PIE1bits.TX1IE= 1; //Enable interrupts!
 				hdlc_sendbuffer(0xCE, 0); //UA
-					crc.Int= 0xffff;
-					crc.Int= crc_1021(crc.Int, address);
-					crc.Int= crc_1021(crc.Int, 0xCE);
 				hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
 				hdlc_sendbuffer(crc.Char[1], 0);
 				hdlc_sendbuffer(DLE, 1);
 				hdlc_sendbuffer(ETX, 1);
 			}
-		} else if ( (received_data[1]&0x37) == 0x02u) { //U-F, disconnect
+		} else if ( (received_data[1]&0x37) == 0x02u) { //U-F: DISC => Disconnect
 			control.NRM= 0u;
 			if ( (received_data[1]&0x08) == 0x08u) { //Check poll flag
 				//Send Unnumbered acknowledge
-				head= tail= 0; //reset buffer
+				//Calculate CRC
+				crc.Int= 0xffff;
+				crc.Int= crc_1021(crc.Int, address);
+				crc.Int= crc_1021(crc.Int, 0xCE);
+				
+				head= tail= 0; //reset send buffer
 				hdlc_sendbuffer(DLE, 1);
 				hdlc_sendbuffer(STX, 1);
 				hdlc_sendbuffer(address, 0);
 				PIE1bits.TX1IE= 1; //Enable interrupts!
 				hdlc_sendbuffer(0xCE, 0); //UA
-					crc.Int= 0xffff;
-					crc.Int= crc_1021(crc.Int, address);
-					crc.Int= crc_1021(crc.Int, 0xCE);
 				hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
 				hdlc_sendbuffer(crc.Char[1], 0);
 				hdlc_sendbuffer(DLE, 1);
 				hdlc_sendbuffer(ETX, 1);
 			}
-		} else if ((received_data[1]&0x37) == 0x06u) { //UA
+		} else if ((received_data[1]&0x37) == 0x06u) { //U-F, ACK => Unnumbered Acknowledge
 			//Do nothing
-		} else if ((received_data[1]&0x37) == 0x21u) { //FRMR
-			//Do nothing	
-		} else if ( (received_data[1]&0x08) == 0x08u) { //Unknown, poll-flag, Frame reject
-			//Send FRMR
-			head= tail= 0;
+		} else if (control.NRM == 1u &&(received_data[1]&0x37) == 0x31u) {//U-F, RSET => Reset 
+			head= tail= 0; //Reset buffer
+		  	receive_sequence_number= send_sequence_number= 0;
+			if ( (received_data[1]&0x08) == 0x08u) { //Check poll flag
+				//Send Unnumbered acknowledge
+				//Calculate CRC
+				crc.Int= 0xffff;
+  				crc.Int= crc_1021(crc.Int, address);
+  				crc.Int= crc_1021(crc.Int, 0xCE);
+  			
+				hdlc_sendbuffer(DLE, 1);
+				hdlc_sendbuffer(STX, 1);
+				hdlc_sendbuffer(address, 0);
+				PIE1bits.TX1IE= 1; //Enable interrupts!
+				hdlc_sendbuffer(0xCE, 0); //UA
+				hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
+			  	hdlc_sendbuffer(crc.Char[1], 0);
+		 		hdlc_sendbuffer(DLE, 1);
+				hdlc_sendbuffer(ETX, 1);
+			}	
+		} else if (control.NRM == 1u && (received_data[1]&0x02) == 0x01u) { //U-F, DISC => Disconnect
+			control.NRM = 0u;
+ 			head= tail= 0; //Reset buffer
+			receive_sequence_number= send_sequence_number= 0;
+			if ( (received_data[1]&0x08) == 0x08u) { //Check poll flag
+				//Send Unnumbered acknowledge
+			  	//Calculate CRC
+			  	crc.Int= 0xffff;
+  				crc.Int= crc_1021(crc.Int, address);
+  				crc.Int= crc_1021(crc.Int, 0xCE);
+  				
+				hdlc_sendbuffer(DLE, 1);
+				hdlc_sendbuffer(STX, 1);
+				hdlc_sendbuffer(address, 0);
+				PIE1bits.TX1IE= 1; //Enable interrupts!
+				hdlc_sendbuffer(0xCE, 0); //UA
+				hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
+			  	hdlc_sendbuffer(crc.Char[1], 0);
+		 		hdlc_sendbuffer(DLE, 1);
+				hdlc_sendbuffer(ETX, 1);
+  			}		  
+		} else if ( control.NRM == 1u && (received_data[1]&0x08) == 0x08u) { //Unknown data AND poll-flag. Send RIM
+			//Send RIM: Request initialisation mode
+			//Calculate CRC
+			crc.Int= 0xffff;
+			crc.Int= crc_1021(crc.Int, address);
+			crc.Int= crc_1021(crc.Int, 0xE8);
+				
+			head= tail= 0; //Reset send buffer
 			hdlc_sendbuffer(DLE, 1);
 			hdlc_sendbuffer(STX, 1);
 			hdlc_sendbuffer(address, 0);
 			PIE1bits.TX1IE= 1; //Enable interrupts!
-			hdlc_sendbuffer(0xE9, 0); //FRMR
-				crc.Int= 0xffff;
-				crc.Int= crc_1021(crc.Int, address);
-				crc.Int= crc_1021(crc.Int, 0xE9);
+			hdlc_sendbuffer(0xE8, 0); //RIM
 			hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
 			hdlc_sendbuffer(crc.Char[1], 0);
 			hdlc_sendbuffer(DLE, 1);
@@ -230,67 +276,10 @@ void hdlc_parseFrame() {  //What do I have to do?
 			
 			if (send_sequence_number == (received_data[1]&0x07) ) { //Correct receive number?
 				//Master received previous frame correct => drop send buffer
-				head= tail= 0u;
-				if ( (received_data[1]&0x08) == 0x08u) { //Receive Ready, used to poll for data
-					if ( (tmp_capt1= io_capt1()) > 0u) { //Send puls data => I-Frame
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= send_sequence_number<<4;
-						send_sequence_number++;
-						send_sequence_number&= 0x07;
-						tmp|= receive_sequence_number;
-						tmp|= 0x08; //Poll-flag
-						tmp= 0x7F&tmp;
-						hdlc_sendbuffer(tmp, 0); //I-Frame
-						hdlc_sendbuffer(tmp_capt1, 0); //Data to transmit
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-							crc.Int= crc_1021(crc.Int, tmp_capt1);
-						hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					} else if ((tmp_input.Int= io_getInputs()) > 0u) { //Send input data => I-frame
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= send_sequence_number<<4;
-						send_sequence_number++;
-						send_sequence_number&= 0x07;
-						tmp|= receive_sequence_number;
-						tmp|= 0x08; //Poll-flag
-						tmp= 0x7F&tmp;
-						hdlc_sendbuffer(tmp, 0); //I-Frame
-						hdlc_sendbuffer(tmp_input.Char[0], 0); //Data to transmit
-						hdlc_sendbuffer(tmp_input.Char[1], 0);
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-							crc.Int= crc_1021(crc.Int, tmp_input.Char[0]);
-							crc.Int= crc_1021(crc.Int, tmp_input.Char[1]);
-						hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					} else { //No data => RR
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= 0x88|receive_sequence_number;
-						hdlc_sendbuffer(tmp, 0); //RR: no data to transmit
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-						hdlc_sendbuffer(crc.Char[0], 0);
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					}
+				//Send our data if we have anything.
+				head= tail= 0u; //Reset send buffer
+				if ( (received_data[1]&0x08) == 0x08u) { //I-F, RR => Receive Ready, used to poll for data
+					hdlc_sendData(); //Transmit pulse count or buttons pressed otherwise send RR
 				}
 			} else { //Resend buffer
 				if ( (received_data[1]&0x08) == 0x08u) {
@@ -299,67 +288,9 @@ void hdlc_parseFrame() {  //What do I have to do?
 			}
 		} else if ( (received_data[1]&0xC0) == 0x80u) { //Supervisory Frame
 			if (send_sequence_number == (received_data[1]&0x07) ) { //Testing sequence numbers from master.
-				head= tail= 0; //reset buffer
+				head= tail= 0; //Reset send buffer
 				if ( (received_data[1]&0x08) == 0x08u) { //Receive Ready, used to poll for data
-					if ( (tmp_capt1= io_capt1()) > 0u) { //Send puls data => I-Frame
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= send_sequence_number<<4;
-						send_sequence_number++;
-						send_sequence_number&= 0x07;
-						tmp|= receive_sequence_number;
-						tmp|= 0x08; //Poll-flag
-						tmp= 0x7F&tmp;
-						hdlc_sendbuffer(tmp, 0); //I-Frame
-						hdlc_sendbuffer(tmp_capt1, 0); //Data to transmit
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-							crc.Int= crc_1021(crc.Int, tmp_capt1);
-						hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					} else if ((tmp_input.Int= io_getInputs()) > 0u) { //Send input data => I-frame
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= send_sequence_number<<4;
-						send_sequence_number++;
-						send_sequence_number&= 0x07;
-						tmp|= receive_sequence_number;
-						tmp|= 0x08; //Poll-flag
-						tmp= 0x7F&tmp;
-						hdlc_sendbuffer(tmp, 0); //I-Frame
-						hdlc_sendbuffer(tmp_input.Char[0], 0); //Data to transmit
-						hdlc_sendbuffer(tmp_input.Char[1], 0);
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-							crc.Int= crc_1021(crc.Int, tmp_input.Char[0]);
-							crc.Int= crc_1021(crc.Int, tmp_input.Char[1]);
-						hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					} else { //No data => RR
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(STX, 1);
-						hdlc_sendbuffer(address, 0);
-						PIE1bits.TX1IE= 1; //Enable interrupts!
-						tmp= 0x88|receive_sequence_number;
-						hdlc_sendbuffer(tmp, 0); //RR: no data to transmit
-							crc.Int= 0xffff;
-							crc.Int= crc_1021(crc.Int, address);
-							crc.Int= crc_1021(crc.Int, tmp);
-						hdlc_sendbuffer(crc.Char[0], 0);
-						hdlc_sendbuffer(crc.Char[1], 0);
-						hdlc_sendbuffer(DLE, 1);
-						hdlc_sendbuffer(ETX, 1);
-					}
+					hdlc_sendData(); //Transmit pulse count or buttons pressed otherwise send RR
 				}
 			} else  { //Resend buffer!!
 				if ( (received_data[1]&0x08) == 0x08u) {
@@ -369,16 +300,18 @@ void hdlc_parseFrame() {  //What do I have to do?
 		}
 	} else { // Not Unnumbered Frame, not in Normal Response Mode
 		if ((received_data[1]&0x08) == 0x08u) { //Check poll flag, if true send data
-			//Send disconnect mode
-			head= tail= 0;
+			//Request initialisation mode
+			//Calculate CRC
+			crc.Int= 0xffff;
+			crc.Int= crc_1021(crc.Int, address);
+			crc.Int= crc_1021(crc.Int, 0xE8);
+			
+			head= tail= 0; //Reset buffer
 			hdlc_sendbuffer(DLE, 1);
 			hdlc_sendbuffer(STX, 1);
 			hdlc_sendbuffer(address, 0);
 			PIE1bits.TX1IE= 1; //Enable interrupts!
-			hdlc_sendbuffer(0xF8, 0); //DM
-				crc.Int= 0xffff;
-				crc.Int= crc_1021(crc.Int, address);
-				crc.Int= crc_1021(crc.Int, 0xF8);
+			hdlc_sendbuffer(0xE8, 0); //RIM
 			hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
 			hdlc_sendbuffer(crc.Char[1], 0);
 			hdlc_sendbuffer(DLE, 1);
@@ -387,11 +320,82 @@ void hdlc_parseFrame() {  //What do I have to do?
 	}
 }
 
+void hdlc_sendData() {
+  if ( (tmp_capt1= io_capt1()) > 0u) { //Send puls data => I-Frame
+		//Control field
+		tmp= send_sequence_number<<4;
+		send_sequence_number++;
+	  	send_sequence_number&= 0x07;
+		tmp|= receive_sequence_number;
+		tmp|= 0x08; //Poll-flag
+	  	tmp= 0x7F&tmp;
+		//Calculate CRC
+		crc.Int= 0xffff;
+		crc.Int= crc_1021(crc.Int, address);
+		crc.Int= crc_1021(crc.Int, tmp);
+		crc.Int= crc_1021(crc.Int, tmp_capt1);
+		
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(STX, 1);
+		hdlc_sendbuffer(address, 0);
+		PIE1bits.TX1IE= 1; //Enable interrupts!
+		hdlc_sendbuffer(tmp, 0); //I-Frame
+		hdlc_sendbuffer(tmp_capt1, 0); //Data to transmit
+		hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
+		hdlc_sendbuffer(crc.Char[1], 0);
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(ETX, 1);
+	} else if ((tmp_input.Int= io_getInputs()) > 0u) { //Send input data => I-frame
+	  	//Control field
+	  	tmp= send_sequence_number<<4;
+		send_sequence_number++;
+		send_sequence_number&= 0x07;
+		tmp|= receive_sequence_number;
+		tmp|= 0x08; //Poll-flag
+		tmp= 0x7F&tmp;
+	  	//Calculate CRC
+	  	crc.Int= 0xffff;
+		crc.Int= crc_1021(crc.Int, address);
+		crc.Int= crc_1021(crc.Int, tmp);
+		crc.Int= crc_1021(crc.Int, tmp_input.Char[0]);
+		crc.Int= crc_1021(crc.Int, tmp_input.Char[1]);
+				
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(STX, 1);
+		hdlc_sendbuffer(address, 0);
+		PIE1bits.TX1IE= 1; //Enable interrupts!
+		hdlc_sendbuffer(tmp, 0); //I-Frame
+		hdlc_sendbuffer(tmp_input.Char[0], 0); //Data to transmit
+		hdlc_sendbuffer(tmp_input.Char[1], 0);
+		hdlc_sendbuffer(crc.Char[0], 0); //FCS!!
+		hdlc_sendbuffer(crc.Char[1], 0);
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(ETX, 1);
+	} else { //No data => RR
+	  	//Control field
+		tmp= 0x88|receive_sequence_number;
+		//Calculate CRC
+		crc.Int= 0xffff;
+		crc.Int= crc_1021(crc.Int, address);
+		crc.Int= crc_1021(crc.Int, tmp);
+		
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(STX, 1);
+		hdlc_sendbuffer(address, 0);
+		PIE1bits.TX1IE= 1; //Enable interrupts!
+		hdlc_sendbuffer(tmp, 0); //RR: no data to transmit
+		hdlc_sendbuffer(crc.Char[0], 0);
+		hdlc_sendbuffer(crc.Char[1], 0);
+		hdlc_sendbuffer(DLE, 1);
+		hdlc_sendbuffer(ETX, 1);
+	}
+}
+
 //Parse incomming byte, search for start of frame and end of frame.
 //If found, parse frame
 void hdlc_receive(unsigned char received_byte) {
-	//Drop data => to many packets!
-	if (current_size >= 0x10u) { //Max size = 16 + 2. (2 from DLE & ETX)
+	//Drop data => to many packets! (Buffer overflow!)
+	if (current_size >= 0x10u) { //Max size = 16 + 2. (2 from DLE & STX)
 		control.SYNC= 0u;
 		control.DLE= 0u;
 		current_size= 0u;
@@ -405,7 +409,7 @@ void hdlc_receive(unsigned char received_byte) {
 			control.SYNC= 1u;
 			control.DLE= 0u;
 			current_size= 0u;
-		} else {control.DLE= 0u;} //Not valid charter
+		} else {control.DLE= 0u;} //Not a valid charter
 	} else {
 		//Save all charters for CRC and handeling of commands
 		receiving_data[current_size]= received_byte;
@@ -413,22 +417,23 @@ void hdlc_receive(unsigned char received_byte) {
 		
 		//End of frame found :):)
 		if (control.DLE == 1u && received_byte == ETX) {
-			io_control_rs485(0); //Enable the led to show we receive some data
 			received_size= current_size - 3; //Remove DLE & ETX, unneeded!
-			if (receiving_data[0] == address) {
-				hdlc_checkFrame();
+ 			if (receiving_data[0] == address) { //For this slave?
+				hdlc_checkFrame(); //Remove dupplicate DLE's and calculate CRC
 				if ( (received_data[received_size-1] == crc.Char[0]) && (received_data[received_size] == crc.Char[1]) ) {
-					hdlc_parseFrame();
-					io_control_rs485(1); //Let the LED blink!
+						hdlc_parseFrame(); //What do we have to do?
+					io_control_rs485(1); //Let the RS485-LED blink to show we are receiving data for this slave.
 				}
+			} else {
+ 				io_control_rs485(0); //Enable the RS485-LED to show we receive some data, but not for this slave.
 			}
 			control.SYNC= 0u; //We have to resync
 		} else if (control.DLE == 1u && received_byte == STX) { //Reset was invalid start of frame!
 			current_size= 0u;
 			control.DLE= 0u;
-		} if (received_byte == DLE) { //Testing for invalid start of frame
+		} if (received_byte == DLE) { //Toggle DLE status
 			control.DLE= !control.DLE;
-		} else {
+		} else {  //Incoming data != DLE => reset DLE status
 			control.DLE= 0u;
 		}
 	}
@@ -440,8 +445,7 @@ void hdlc_checkFrame() {
 	unsigned int i, offset;
 	offset= 0;
 	crc.Int= 0xffff;
-	
-	
+		
 	//Remove double DLE's
 	for (i=0; i <= received_size; i++) {
 		if (receiving_data[i] == DLE && receiving_data[i+1] == DLE) { // If DLE, skip one place
@@ -450,7 +454,7 @@ void hdlc_checkFrame() {
 		received_data[i-offset]= receiving_data[i];
 	}
 	
-	//Set receiving_size to correct site.
+	//Set receiving_size to correct size.
 	received_size-= offset;
 	
 	//Calculate CRC
